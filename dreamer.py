d8f39647e87936d42bc4e1edb8d2c059438025cc
nathan <nathanclaret.pro@gmail.com>
2026-04-15 22:23:53 -0400

    first step of the algorithm

---
+++ dreamer.py
@@ -0,0 +1,36 @@
+import numpy as np 
+
+from dm_control import suite
+from dm_control.suite.wrappers import pixels
+
+
+def train(env, nb_seeds: int = 5, ):
+
+    # ------------- Generate and store sample of (action, state, reward) from random action -------------
+    action_space_spec = env.action_spec()
+    time_step = env.reset()
+    random_state_dataset = []
+    for seed in range(nb_seeds):
+        print(f"Genereting 1000 sample with seed : {seed}")
+        while not time_step.last():
+            np.random.seed(seed)
+            random_action = np.random.uniform(low=action_space_spec.minimum, high=action_space_spec.maximum, size=action_space_spec.shape)
+            time_step = env.step(random_action)
+            sample = (random_action, time_step.observation["pixels"], time_step.reward)
+            random_state_dataset.append(sample)
+        time_step = env.reset()
+
+    return random_state_dataset
+
+
+if __name__ == "__main__":
+
+    print("Environment creation ...")
+    env = suite.load(domain_name="cartpole", task_name="swingup")
+    env = pixels.Wrapper(env=env, render_kwargs={"height": 64, "width": 64, "camera_id": 0})
+    print("Environment created\n")
+
+
+    test = train(env=env)
+    print(len(test))
+    print(test[0])
